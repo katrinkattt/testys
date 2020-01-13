767ee059699fb16a95745695195b097e7ffc3388
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2020-01-14 01:28:11 +0500

    close btn is ready

--- testy/src/index.js
+++ testy/src/index.js
@@ -6,9 +6,16 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-const btn = document.getElementById('btnClick')
-        const modal = document.getElementById('modal')
-        btn.addEventListener('click', (display) =>{
-            modal.style.display='block'
-        });
+const btn = document.getElementById('btnClick');
+const modal = document.getElementById('modal');
+const btnClose = document.getElementById('btnClose');
+
+btn.addEventListener('click', () => {
+    modal.style.display='block';
+});
+
+btnClose.addEventListener('click', () => {
+    modal.style.display='none';
+})
+
 serviceWorker.unregister();
