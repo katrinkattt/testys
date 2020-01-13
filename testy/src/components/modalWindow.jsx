3048ed65ee7b152c676bcc251a44e371fa188b2b
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2020-01-14 01:05:32 +0500

    setting for close modalWindow

--- testy/src/components/modalWindow.jsx
+++ testy/src/components/modalWindow.jsx
@@ -5,7 +5,10 @@ export default function ModalWindow(props){
         <div id="modal" className="modal__main">
             <div className="modal__title">
                 <div className="modal__text__title">{props.title}</div>
-                <div id="btnClose" className="modal__close">X</div>
+                <div id="btnClose" className="modal__close">
+                    <div className="modal__close__line"></div>
+                    <div className="modal__close__line1"></div>
+                </div>
             </div>
             <div className="modal__text">{props.text}</div>
             <div className="modal__footer"></div>
