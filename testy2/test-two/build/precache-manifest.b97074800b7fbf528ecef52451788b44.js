32be2614eee4f18fca908dcb4abba135e1094298
Nkitina Ekaterina <52584077+katrinnnKat@users.noreply.github.com>
2020-01-20 13:33:51 +0500

    ruild ready

---
+++ testy2/test-two/build/precache-manifest.b97074800b7fbf528ecef52451788b44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c00359ca574acc5a6185e300399cdce9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30bdb1065abd1ddb82b2",
+    "url": "/static/css/main.bff74e23.chunk.css"
+  },
+  {
+    "revision": "e464fa938da36222e50f",
+    "url": "/static/js/2.c872db35.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c872db35.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30bdb1065abd1ddb82b2",
+    "url": "/static/js/main.d41c37ff.chunk.js"
+  },
+  {
+    "revision": "1625994d74e3ea8a44b9",
+    "url": "/static/js/runtime-main.c6e41a7f.js"
+  }
+]);
